767c0078a34bb66ed0aa9880fb91f7d7d325f8d3
ShixiangWang <wangshx@shanghaitech.edu.cn>
2021-11-08 19:48:03 +0800

    give it a try

---
+++ cli/grrs/src/main.rs
@@ -0,0 +1,28 @@
+#![allow(unused)]
+use structopt::StructOpt;
+
+/// Search for a pattern in a file and 
+/// display the lines that contain it.
+#[derive(StructOpt)]
+struct Cli {
+    /// The pattern to look for
+    pattern: String,
+    /// The path to the file to read
+    #[structopt(parse(from_os_str))]
+    path: std::path::PathBuf,
+}
+
+fn main() {
+    let args = Cli::from_args();
+    let result = std::fs::read_to_string(&args.path);
+    let content = match result {
+        Ok(content) => content,
+        Err(error) => panic!("Can't deal with {}, just exit here", error),
+    };
+    
+    for line in content.lines() {
+        if line.contains(&args.pattern) {
+            println!("{}", line);
+        }
+    }
+}
